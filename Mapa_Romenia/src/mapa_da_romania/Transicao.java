892720fd149ba8bfa7ed43c9478e2e637d210df8
Danilo Carneiro <danilocarneiro.t26@gmail.com>
2022-11-14 18:28:59 -0300

    Projeto de IA(Vacuum Word)

---
+++ Mapa_Romenia/src/mapa_da_romania/Transicao.java
@@ -0,0 +1,28 @@
+package mapa_da_romania;
+
+public class Transicao {
+
+	private Estado estadoDest;
+	private int custoAresta;
+	
+	
+	public Transicao(Estado estadoDest, int custoAresta) {
+		this.estadoDest = estadoDest;
+		this.custoAresta = custoAresta;
+	}
+	
+	public Estado getEstadoDest() {
+		return estadoDest;
+	}
+	public void setEstadoDest(Estado estadoDest) {
+		this.estadoDest = estadoDest;
+	}
+	public int getCustoAresta() {
+		return custoAresta;
+	}
+	public void setCustoAresta(int custoAresta) {
+		this.custoAresta = custoAresta;
+	}
+	
+	
+}
